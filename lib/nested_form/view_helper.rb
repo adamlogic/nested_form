b533f82b1ba379b177c1446c2887fbf4debaa6f6
Alexander [Cheba] Mankuta <cheba@pointlessone.org>
2010-02-19 21:10:54 +0300

    Removed trailing spaces

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -2,13 +2,13 @@ module NestedForm
   module ViewHelper
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
-      form_for(*(args << options), &block) 
+      form_for(*(args << options), &block)
       @after_nested_form_callbacks ||= []
       @after_nested_form_callbacks.each do |callback|
         callback.call
       end
     end
-    
+
     def after_nested_form(&block)
       @after_nested_form_callbacks ||= []
       @after_nested_form_callbacks << block
