b10e1a501a4b8bdbd84936a2df7b3d2007e54782
Alexander [Cheba] Mankuta <cheba@pointlessone.org>
2010-02-19 21:10:08 +0300

    No inline JS

--- lib/nested_form/builder.rb
+++ lib/nested_form/builder.rb
@@ -8,20 +8,20 @@ module NestedForm
         fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
         @template.concat('</div>')
       end
-      @template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
+      @template.link_to(name, "#", :class => "add_nested_fields", "data-association" => association)
     end
-    
+
     def link_to_remove(name)
-      hidden_field(:_destroy) + @template.link_to(name, "javascript:void(0)", :class => "remove_nested_fields")
+      hidden_field(:_destroy) + @template.link_to(name, "#", :class => "remove_nested_fields")
     end
-    
+
     def fields_for_with_nested_attributes(association, args, block)
       @fields ||= {}
       @fields[association] = block
       super
     end
 
-    
+
     def fields_for_nested_model(name, association, args, block)
       @template.concat('<div class="fields">')
       super
--- spec/nested_form/builder_spec.rb
+++ spec/nested_form/builder_spec.rb
@@ -10,11 +10,11 @@ describe NestedForm::Builder do
     end
 
     it "should have an add link" do
-      @builder.link_to_add("Add", :tasks).should == '<a href="javascript:void(0)" class="add_nested_fields" data-association="tasks">Add</a>'
+      @builder.link_to_add("Add", :tasks).should == '<a href="#" class="add_nested_fields" data-association="tasks">Add</a>'
     end
 
     it "should have a remove link" do
-      @builder.link_to_remove("Remove").should == '<input id="item__destroy" name="item[_destroy]" type="hidden" /><a href="javascript:void(0)" class="remove_nested_fields">Remove</a>'
+      @builder.link_to_remove("Remove").should == '<input id="item__destroy" name="item[_destroy]" type="hidden" /><a href="#" class="remove_nested_fields">Remove</a>'
     end
 
     it "should wrap nested fields each in a div with class" do
